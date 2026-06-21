85c8ab9790aeadf4641956ede43dce0aeb3271ee
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2026-06-21 11:20:05 +0200

    chore: add workspace 10 bindings to hyprland

--- users/mirza/.config/hypr/overrides/binds.lua
+++ users/mirza/.config/hypr/overrides/binds.lua
@@ -1,1 +1,5 @@
 hl.bind("SUPER + Return", hl.dsp.exec_cmd("ghostty"))
+
+-- Add workspace 10
+hl.bind("SUPER + 0", hl.dsp.focus({ workspace = "10" }))
+hl.bind("SUPER + SHIFT + 0", hl.dsp.window.move({ workspace = "10" }))
